c36dea8d9f904127539add95e052c3f8716f2122
Nikola Pavlov <nikola.pavlov1955@gmail.com>
2016-03-18 23:21:44 +0200

    change variable names in setup_v2.py

--- setup_v2.py
+++ setup_v2.py
@@ -1,22 +1,20 @@
 import os
-import errno
 
 
 CWD = os.getcwd()
 HOME_DIR = os.path.expanduser('~')
 VUNDLE_GIT_REPO = 'https://github.com/VundleVim/Vundle.vim.git'
-VUNDLE_REPO_DESTINATION = HOME_DIR + '/.vim/bundle/Vundle.vim'
-I3_CONFIG_DIR = HOME_DIR + '/' + '.i3'
-ZATHURA_CONFIG_DIR = HOME_DIR + '/' + '.config/zathura'
-
+VUNDLE_DESTINATION = HOME_DIR + '/.vim/bundle/Vundle.vim'
+I3_DESTINATION_FOLDER = HOME_DIR + '/' + '.i3'
+ZATHURA_DESTINATION_FOLDER = HOME_DIR + '/' + '.config/zathura'
 BASHRC_FILE = CWD + '/' + 'bashrc'
-BASHRC_FILE_DESTINATION = HOME_DIR + '/' + '.bashrc'
+BASHRC_DESTINATION = HOME_DIR + '/' + '.bashrc'
 VIMRC_FILE = CWD + '/' + 'vimrc'
-VIMRC_FILE_DESTINATION = HOME_DIR + '/' + '.vimrc'
+VIMRC_DESTINATION = HOME_DIR + '/' + '.vimrc'
 I3_CONFIG_FILE = CWD + '/' + 'config'
-I3_CONFIG_FILE_DESTINATION = HOME_DIR + '/' + '.i3/config'
+I3_DESTINATION = HOME_DIR + '/' + '.i3/config'
 ZATHURA_FILE = CWD + '/' + 'zathurarc'
-ZATHURA_FILE_DESTINATION = HOME_DIR + '/' + '.config/zathura/zathurarc'
+ZATHURA_DESTINATION = HOME_DIR + '/' + '.config/zathura/zathurarc'
 
 
 def create_symlink(from_file, to_file):
@@ -25,20 +23,20 @@ def create_symlink(from_file, to_file):
         os.symlink(from_file, to_file)
         print('replace file with symlink ' + to_file)
     else:
-        if not os.path.exists(I3_CONFIG_DIR):
-            os.mkdir(I3_CONFIG_DIR)
-        if not os.path.exists(ZATHURA_CONFIG_DIR):
-            os.mkdir(ZATHURA_CONFIG_DIR)
+        if not os.path.exists(I3_DESTINATION_FOLDER):
+            os.mkdir(I3_DESTINATION_FOLDER)
+        if not os.path.exists(ZATHURA_DESTINATION_FOLDER):
+            os.mkdir(ZATHURA_DESTINATION_FOLDER)
         os.symlink(from_file, to_file)
         print('created symlink ' + to_file)
 
 
-create_symlink(BASHRC_FILE, BASHRC_FILE_DESTINATION)
-create_symlink(VIMRC_FILE, VIMRC_FILE_DESTINATION)
-create_symlink(I3_CONFIG_FILE, I3_CONFIG_FILE_DESTINATION)
-create_symlink(ZATHURA_FILE, ZATHURA_FILE_DESTINATION)
+create_symlink(BASHRC_FILE, BASHRC_DESTINATION)
+create_symlink(VIMRC_FILE, VIMRC_DESTINATION)
+create_symlink(I3_CONFIG_FILE, I3_DESTINATION)
+create_symlink(ZATHURA_FILE, ZATHURA_DESTINATION)
 
+# install vundle if not present in the current system
 if os.path.exists(HOME_DIR + '/.vim/bundle/Vundle.vim'):
     print('vundle is already instaled!')
-else:
-    os.system('git clone ' + VUNDLE_GIT_REPO + ' ' + VUNDLE_REPO_DESTINATION)
+else: os.system('git clone ' + VUNDLE_GIT_REPO + ' ' + VUNDLE_DESTINATION)
