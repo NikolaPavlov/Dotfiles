99e8397eb82f66a4a0f22b333770818a1c582f10
Nikola Pavlov <nikola.pavlov1955@gmail.com>
2016-05-19 19:41:32 +0300

    fix ssh error in Scripts/backup.sh

--- Scripts/backup.sh
+++ Scripts/backup.sh
@@ -4,7 +4,7 @@
 # $2 backup destination directory
 # notes: 
 # run it with sudo
-# rsync [stick your options here] / user@backup-server:/path/to/backups
+# rsync  / user@backup-server:/path/to/backups
 # echo 'rsync [dir_for_backup] [dir to backup]'
 
 rsync -aAXv --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*/.gvfs"} $1 $2
