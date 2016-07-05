fb2a32d32d7167db31a464d447826541ff076b87
NikolaPavlov <nikola.pavlov1955@gmail.com>
2016-07-05 19:42:39 +0300

    refactor setup.py remove vim from it, remake on neovimrc init.vim

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import os
 CWD = os.getcwd()
 HOME_DIR = os.path.expanduser('~')
 
-VUNDLE_GIT_REPO = 'https://github.com/VundleVim/Vundle.vim.git'
-VUNDLE_DESTINATION = HOME_DIR + '/.vim/bundle/Vundle.vim'
+# VUNDLE_GIT_REPO = 'https://github.com/VundleVim/Vundle.vim.git'
+# VUNDLE_DESTINATION = HOME_DIR + '/.vim/bundle/Vundle.vim'
 
 BASHRC_FILE = CWD + '/bashrc'
 BASHRC_DESTINATION = HOME_DIR + '/.bashrc'
 
-VIMRC_FILE = CWD + '/vim/vimrc'
-VIMRC_DESTINATION = HOME_DIR + '/.vimrc'
+#VIMRC_FILE = CWD + '/vim/vimrc'
+#VIMRC_DESTINATION = HOME_DIR + '/.vimrc'
 
 NEOVIM_NVIMRC_FILE = CWD + '/neovim/init.vim'
 NEOVIM_NVIMRC_DESTINATION = HOME_DIR + '/.config/nvim/init.vim'
@@ -29,10 +29,10 @@ PHONETIC = CWD + '/Scripts/phonetic_keys.sh'
 PHONETIC_DESTINATION = HOME_DIR + '/.phonetic_keys.sh'
 
 # install vundle if not present in the current system
-if os.path.exists(HOME_DIR + '/.vim/bundle/Vundle.vim'):
-    print('vundle is already instaled!')
-else:
-    os.system('git clone ' + VUNDLE_GIT_REPO + ' ' + VUNDLE_DESTINATION)
+# if os.path.exists(HOME_DIR + '/.vim/bundle/Vundle.vim'):
+#     print('vundle is already instaled!')
+# else:
+#     os.system('git clone ' + VUNDLE_GIT_REPO + ' ' + VUNDLE_DESTINATION)
 
 
 
@@ -60,7 +60,7 @@ def create_symlink(from_file, to_file):
 
 create_dirs_if_doesnt_exists()
 create_symlink(BASHRC_FILE, BASHRC_DESTINATION)
-create_symlink(VIMRC_FILE, VIMRC_DESTINATION)
+#create_symlink(VIMRC_FILE, VIMRC_DESTINATION)
 create_symlink(NEOVIM_NVIMRC_FILE, NEOVIM_NVIMRC_DESTINATION)
 create_symlink(I3_CONFIG_FILE, I3_DESTINATION)
 create_symlink(ZATHURA_FILE, ZATHURA_DESTINATION)
