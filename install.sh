04b3b077706ff9eae27a9be927a7cdc1bfcef37f
Nikola Pavlov <nikola.pavlov1955@gmail.com>
2019-02-15 16:42:08 +0200

    refactoring dotfiles

--- install.sh
+++ install.sh
@@ -7,10 +7,12 @@ EXTRA_DIR="$HOME/.extra"
 
 
 ln -sfv "$DOTFILES_DIR/.bashrc" ~/
-ln -sfv "$DOTFILES_DIR/Neovim/init.vim" ~/.config/nvim/init.vim
-ln -sfv "$DOTFILES_DIR/i3" ~/.config/i3
+# create /.config/nvim
+ln -sfv "$DOTFILES_DIR/nvim/init.vim" ~/.config/nvim/init.vim
+ln -sfv "$DOTFILES_DIR/i3" ~/.config/
+# create ~/.config/polybar
 ln -sfv "$DOTFILES_DIR/Polybar/config" ~/.config/polybar/config
 ln -sfv "$DOTFILES_DIR/Zathura" ~/.config/zathura
-ln -sfv "$DOTFILES_DIR/Git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/Git/.gitconfig" ~/
 ln -sfv "$DOTFILES_DIR/Irssi" ~/.irssi
 ln -sfv "$DOTFILES_DIR/Scripts/phonetic_keys.sh" ~/.phonetic_keys
