b88de61b1cb9bb5a0ac67c1d6a84df1e56d3d064
Nikola Pavlov <nikola.pavlov1955@gmail.com>
2021-11-06 09:36:09 +0200

    up

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -102,6 +102,8 @@ return require('packer').startup({function(use)
   use 'majutsushi/tagbar'            -- display tags with <T>
   use 'godlygeek/tabular'            -- text align
   use 'dbeniamine/cheat.sh-vim'      -- query cht.sh with :Cheat
+  use 'airblade/vim-rooter'          -- autochange workdir (<leader>G / <leader>f as alternative ?)
+
   -- TODO: rm use 'ludovicchabant/vim-gutentags'    -- Automatic tags management
   -- TODO: install tags (T) plugin
   -- TODO: mbbill/undotree
