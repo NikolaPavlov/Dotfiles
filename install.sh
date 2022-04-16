c6dd1aed828eb3f6cd6f01ae8f1b8df09a04da80
Nikola Pavlov <nikola.pavlov1955@gmail.com>
2022-04-16 18:51:33 +0300

    fix nvim install in install.sh\

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ EXTRA_DIR="$HOME/.extra"
 
 
 mkdir -p $HOME/.config/alacritty
-mkdir -p $HOME/.config/nvim/undo_dir
 mkdir -p $HOME/.config/zathura
 mkdir -p $HOME/.config/qtile
 mkdir -p $HOME/.config/tmux
@@ -16,7 +15,7 @@ mkdir -p $HOME/.gnupg
 ln -sfv "$DOTFILES_DIR/alacritty/alacritty.yml" ~/.config/alacritty/alacritty.yml
 ln -sfv "$DOTFILES_DIR/.bashrc" $HOME
 ln -sfv "$DOTFILES_DIR/.bash_profile" $HOME
-ln -sfv "$DOTFILES_DIR/nvim/lua" ~/.config/nvim/lua
+ln -sfv "$DOTFILES_DIR/nvim" ~/.config
 ln -sfv "$DOTFILES_DIR/Zathura/zathurarc" ~/.config/zathura/zathurarc
 ln -sfv "$DOTFILES_DIR/Git/.gitconfig" $HOME
 ln -sfv "$DOTFILES_DIR/Scripts" $HOME
@@ -26,3 +25,5 @@ ln -sfv "$DOTFILES_DIR/qtile/autostart.sh" ~/.config/qtile
 ln -sfv "$DOTFILES_DIR/gnupg/gpg-agent.conf" ~/.gnupg
 ln -sfv "$DOTFILES_DIR/tmux/tmux.conf" ~/.config/tmux
 ln -sfv "$DOTFILES_DIR/.inputrc" $HOME
+
+mkdir -p $HOME/.config/nvim/undo_dir
