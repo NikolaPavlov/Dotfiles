cdb36ae302c9eb40992c1c546af814eacfd43018
Nikola Pavlov <nikola.pavlov1955@gmail.com>
2024-08-27 11:54:17 +0300

    up

--- nvim/lua/gh/plugins/cmp.lua
+++ nvim/lua/gh/plugins/cmp.lua
@@ -44,19 +44,19 @@ return {
 				completion = { completeopt = "menu,menuone,noinsert" },
 				-- No, but seriously. Please read `:help ins-completion`, it is really good!
 				mapping = cmp.mapping.preset.insert({
-					["<C-j>"] = cmp.mapping.select_next_item(),
-					["<C-k>"] = cmp.mapping.select_prev_item(),
+					["<C-l>"] = cmp.mapping.select_next_item(),
+					["<C-h>"] = cmp.mapping.select_prev_item(),
 					["<Tab>"] = cmp.mapping.confirm({ select = true }),
 					-- ["<C-n>"] = cmp.mapping.select_next_item(),
 					-- ["<C-p>"] = cmp.mapping.select_prev_item(),
 					-- ["<C-y>"] = cmp.mapping.confirm({ select = true }),
 
-					["<C-l>"] = cmp.mapping(function()
+					["<C-j>"] = cmp.mapping(function()
 						if luasnip.expand_or_locally_jumpable() then
 							luasnip.expand_or_jump()
 						end
 					end, { "i", "s" }),
-					["<C-h>"] = cmp.mapping(function()
+					["<C-k>"] = cmp.mapping(function()
 						if luasnip.locally_jumpable(-1) then
 							luasnip.jump(-1)
 						end
