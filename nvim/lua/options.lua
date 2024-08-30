b0071397144e89d43905ace667e63d5a8dd52ebe
Nikola Pavlov <nikola.pavlov1955@gmail.com>
2024-08-30 20:50:59 +0300

    mini.nvim plugins added for testing

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -87,10 +87,9 @@ local disabled_built_ins = {
 for _, plugin in pairs(disabled_built_ins) do
 	vim.g["loaded_" .. plugin] = 1
 end
--- vim: ts=2 sts=2 sw=2 et
 
-
--- force to use OSC52 # TODO: lag when copy/paste 
+-- # TODO: super slow
+-- # TODO: force to use OSC52
 -- vim.g.clipboard = {
 --   name = 'OSC 52',
 --   copy = {
@@ -102,3 +101,5 @@ end
 --     ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
 --   },
 -- }
+
+-- vim: ts=2 sts=2 sw=2 et
