efe4708ab30b309a101c6530fc6591391b2459ce
Nikola Pavlov <nikola.pavlov1955@gmail.com>
2024-08-30 06:49:37 +0300

    up

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -12,7 +12,7 @@ require("lazy").setup({
 	require("gh/plugins/lspconfig"),
 	-- require("gh/plugins/conform"),
 	require("gh/plugins/cmp"),
-	-- require("gh/plugins/tokyonight"),
+	require("gh/plugins/tokyonight"),
 	require("gh/plugins/badwolf"),
 	require("gh/plugins/lualine"),
 	require("gh/plugins/todo-comments"),
