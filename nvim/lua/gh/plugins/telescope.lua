fe2d816d03f918e92be7dcb36e83219ef206dc6a
Nikola Pavlov <nikola.pavlov1955@gmail.com>
2025-01-12 16:11:30 +0200

    switch from snipe to telescope buff

--- nvim/lua/gh/plugins/telescope.lua
+++ nvim/lua/gh/plugins/telescope.lua
@@ -71,7 +71,7 @@ return {
 
       local builtin = require("telescope.builtin")
 
-      -- vim.keymap.set("n", "<leader>b",  builtin.buffers)
+      vim.keymap.set("n", "<leader>b",  builtin.buffers)
       -- vim.keymap.set("n", "<leader>f",  builtin.git_files, { desc = "Search [F]iles" })
       vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Search [F]iles" })
       vim.keymap.set("n", "<leader>sb", builtin.git_branches, { desc = "Search [G]it [B]ranches" })
