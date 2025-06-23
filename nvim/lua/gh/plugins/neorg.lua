3d5ef3e16d41cfc0ec0098d3cbd32063d41a16ad
Nikola Pavlov <nikola.pavlov1955@gmail.com>
2025-06-23 07:44:26 +0300

    neorg setup

--- nvim/lua/gh/plugins/neorg.lua
+++ nvim/lua/gh/plugins/neorg.lua
@@ -1,12 +1,16 @@
 return {
   "nvim-neorg/neorg",
   version = "*",
+  lazy = false,
 
   config = function()
     require("neorg").setup({
       load = {
         ["core.defaults"] = {},
         ["core.concealer"] = {}, -- adds prety icons to your documents
+        ["core.autocommands"] = {},
+        ["core.integrations.treesitter"] = {},
+        ["core.syntax"] = {}, -- uses for code blocks
         ["core.dirman"] = { -- manage Neorg  workspaces
           config = {
             workspaces = {
