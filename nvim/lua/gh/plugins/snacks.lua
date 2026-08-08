173edbec526fcf7f7f7a635eb02c3623428876ac
Nikola Pavlov <nikola.pavlov1955@gmail.com>
2026-08-08 11:30:12 +0300

    claude fix tmux

--- nvim/lua/gh/plugins/snacks.lua
+++ nvim/lua/gh/plugins/snacks.lua
@@ -48,7 +48,7 @@ return {
       },
       picker = { enabled = true },
       quickfile = { enabled = true },
-      -- scroll = { enabled = true },
+      scroll = { enabled = true },
       statuscolumn = { enabled = true },
       words = { enabled = true },
       scope = { enabled = true },
